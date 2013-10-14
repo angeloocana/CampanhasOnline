cabafe4df5371670b28c2bcce1298e04618a0f90
angeloocana <angeloocana@gmail.com>
2013-10-14 18:53:00 -0300

    Criação do Produto de teste

---
+++ src/Orchard.Web/Modules/TotalOnDemand.CampanhasOnline/Drivers/ProductDriver.cs
@@ -0,0 +1,36 @@
+﻿using TotalOnDemand.CampanhasOnline.Models;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement;
+
+namespace TotalOnDemand.CampanhasOnline.Drivers
+{
+    public class ProductDriver : ContentPartDriver<ProductPart>
+    {
+        protected override DriverResult Display(
+            ProductPart part, string displayType, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Product",
+                () => shapeHelper.Parts_Product(
+                    Sku: part.Sku,
+                    Price: part.Price));
+        }
+
+        //GET
+        protected override DriverResult Editor(ProductPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Product_Edit",
+                () => shapeHelper.EditorTemplate(
+                    TemplateName: "Parts/Product",
+                    Model: part,
+                    Prefix: Prefix));
+        }
+
+        //POST
+        protected override DriverResult Editor(
+            ProductPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/TotalOnDemand.CampanhasOnline/Handlers/ProductHandler.cs
@@ -0,0 +1,12 @@
+﻿using Orchard.ContentManagement.Handlers;
+using TotalOnDemand.CampanhasOnline.Models;
+using Orchard.Data;
+
+namespace TotalOnDemand.CampanhasOnline.Handlers
+{
+    public class ProductHandler : ContentHandler {
+        public ProductHandler(IRepository<ProductPartRecord> repository) {
+            Filters.Add(StorageFilter.For(repository));
+        }
+    }
+}
